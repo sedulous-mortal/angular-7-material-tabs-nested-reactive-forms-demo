226fccaabb7d8d22b460ab7047f3bb730eef3166
sedulous-mortal <alisonnicolestuart@gmail.com>
2019-08-26 11:30:38 -0400

    might have broken it by capturing name data change as event and setting local tabscomponent var to the event

--- src/app/tabs.component.ts
+++ src/app/tabs.component.ts
@@ -1,10 +1,16 @@
 import { Input, Component } from '@angular/core';
-import { Person } from './person-data.model';
+import { Person, Name } from './person-data.model';
 
 @Component({
   selector: 'tabs-component',
   templateUrl: './tabs.component.html'
 })
+
 export class TabsComponent  {
    @Input() personData: Person;
+   nameData: Name;
+
+   captureNameData(event){
+     this.nameData = event;
+   }
 }
